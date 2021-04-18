cdae20ad9d067f1512399e94ae0d50746025b20c
ryan <ryanyogan@gmail.com>
2021-04-18 10:09:45 -0500

    extracted form validation and error logic to a new package

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"yogan.dev/snippetbox/pkg/forms"
 	"yogan.dev/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
